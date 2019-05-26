c9d0bbf8e173d9acc88c25635afc3e6dce015740
L-Train <train19@gmail.com>
2019-05-26 17:45:03 -0400

    FF v1.0

---
+++ app/data/friends.js
@@ -0,0 +1,176 @@
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+
+let friendsArray = [
+    {
+        "name": "Ahmed",
+        "photo": "friend1.png",
+        "scores": [
+            "1",
+            "2",
+            "5",
+            "4",
+            "1", 
+            "5",
+            "1",
+            "4",
+            "4",
+            "5"
+
+        ]
+    },
+    {
+        "name": "Vyjoo",
+        "photo": "friend2.jpg",
+        "scores": [
+            "5",
+            "1",
+            "3",
+            "4",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Magesh",
+        "photo": "friend3.jpg",
+        "scores": [ 
+            "2",
+            "1",
+            "3",
+            "2",
+            "4",
+            "2",
+            "5",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Prakash",
+        "photo": "friend4.jpg",
+        "scores": [
+            "3",
+            "2",
+            "5",
+            "5",
+            "4",
+            "2",
+            "1",
+            "4",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Alisha",
+        "photo": "friend5.jpg",
+        "scores": [
+            "5",
+            "2",
+            "5",
+            "4",
+            "3",
+            "4",
+            "1",
+            "4",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Anika",
+        "photo": "friend6.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "4",
+            "1",
+            "4",
+            "2",
+            "2",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Vaneet",
+        "photo": "friend7.jpg",
+        "scores": [
+            "1",
+            "5",
+            "3",
+            "1",
+            "2",
+            "5",
+            "1",
+            "5",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Aanan",
+        "photo": "friend8.jpg",
+        "scores": [
+            "4",
+            "4",
+            "3",
+            "2",
+            "4",
+            "4",
+            "5",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Ali",
+        "photo": "friend9.jpg",
+        "scores": [
+            "3",
+            "2",
+            "4",
+            "2",
+            "1",
+            "4",
+            "3",
+            "4",
+            "1",
+            "3"
+        ]
+    },
+    {
+        "name": "Anish",
+        "photo": "friend10.jpg",
+        "scores": [
+            "2",
+            "1",
+            "2",
+            "3",
+            "2",
+            "1",
+            "2",
+            "1",
+            "1",
+            "1"
+        ]
+    }
+]
+
+function findFriend () {
+
+
+
+}
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
